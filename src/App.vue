ba804fed2d4f8c78afdc09e336e2337976ca88ae
Brice <lebowvsky@gmail.com>
2022-08-29 12:11:43 +0200

    Add ScoreBoard

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    <ScoreBoard :winCount="this.winCount" :loseCount="this.loseCount" />
     <template v-if="this.question">
       <h1 v-html="this.question"></h1>
       <template v-for="answer in this.answers" :key="answer">
@@ -18,16 +19,25 @@
 
       <section v-if="this.answerSubmited" class="result">
         <h4 v-if="this.chosenAnswer == this.correctAnswer">&#9989; Congratulation, the answer is good !!!</h4>
-        <h4 v-else>&#10060; I'm sorry, youpicked the wrong answer. The correct answer was {{ this.correctAnswer }}</h4>
-        <button class="send">NextQuestion</button>
+        <h4 v-else>
+          &#10060; I'm sorry, youpicked the wrong answer. The correct answer was "<span
+            v-html="this.correctAnswer"
+          ></span
+          >"
+        </h4>
+        <button class="send" type="button" @click="this.getNewQuestion()">NextQuestion</button>
       </section>
     </template>
   </div>
 </template>
 
 <script>
+import ScoreBoard from "@/components/ScoreBoard.vue";
 export default {
   name: "App",
+  components: {
+    ScoreBoard,
+  },
   data() {
     return {
       question: undefined,
@@ -35,6 +45,8 @@ export default {
       correctAnswer: undefined,
       chosenAnswer: undefined,
       answerSubmited: false,
+      winCount: 0,
+      loseCount: 0,
     };
   },
   computed: {
@@ -45,23 +57,31 @@ export default {
     },
   },
   methods: {
+    async getNewQuestion() {
+      this.answerSubmited = false;
+      this.chosenAnswer = undefined;
+      this.question = undefined;
+
+      const response = await this.axios.get("https://opentdb.com/api.php?amount=1&category=18");
+      this.question = response.data.results[0].question;
+      this.incorrectAnswers = response.data.results[0].incorrect_answers;
+      this.correctAnswer = response.data.results[0].correct_answer;
+    },
     submitAnswer() {
       if (!this.chosenAnswer) {
         alert("Chose an answer !");
       } else {
+        this.answerSubmited = true;
         if (this.chosenAnswer === this.correctAnswer) {
-          this.answerSubmited = true;
+          this.winCount++;
         } else {
-          this.answerSubmited = true;
+          this.loseCount++;
         }
       }
     },
   },
-  async created() {
-    const response = await this.axios.get("https://opentdb.com/api.php?amount=1&category=18");
-    this.question = response.data.results[0].question;
-    this.incorrectAnswers = response.data.results[0].incorrect_answers;
-    this.correctAnswer = response.data.results[0].correct_answer;
+  created() {
+    this.getNewQuestion();
   },
 };
 </script>
---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,34 @@
+<template>
+  <section class="score">
+    Player<span>{{ this.winCount }}</span
+    >x<span>{{ this.loseCount }}</span
+    >Computer
+  </section>
+  <hr />
+</template>
+<script>
+export default {
+  props: {
+    winCount: Number,
+    loseCount: Number,
+  },
+};
+</script>
+<style lang="scss">
+.score {
+  font-size: 2rem;
+  margin: 100px 0 30px 0;
+  span {
+    display: inline-block;
+    padding: 0 5px;
+    margin: 0 5px;
+    text-align: center;
+    border: 1px solid grey;
+    font-weight: 700;
+  }
+}
+
+hr {
+  margin: 0 0 50px 0;
+}
+</style>
